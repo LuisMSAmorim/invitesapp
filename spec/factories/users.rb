0a7a06248676a2823e64739fa39b24e52ef1897f
luisMSAmorim <luismiguelsilvaamorim@gmail.com>
2024-02-26 15:02:48 -0300

    test: user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :user do
-    
+    sequence :email do |n|
+      "person#{n}@example.com"
+    end
+    role { 'user' }
+    trait :admin do
+      role { 'admin' }
+    end
   end
 end
