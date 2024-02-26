be2282d0f21688c28c0e88fe43a814a44300b6e3
luisMSAmorim <luismiguelsilvaamorim@gmail.com>
2024-02-26 15:40:26 -0300

    test: add bad way in the create administrator use case

--- spec/use_cases/administrators/create_spec.rb
+++ spec/use_cases/administrators/create_spec.rb
@@ -7,11 +7,20 @@ RSpec.describe Administrators::Create, type: :use_case do
   subject { described_class.new(params) }
 
   describe '#call' do
-    it 'creates an administrator' do
-      user_administrator = subject.call
-
-      expect(user_administrator).to be_persisted
-      expect(user_administrator.role).to eq('admin')
+    context "when the params are valid" do
+      it 'creates an administrator' do
+        user_administrator = subject.call
+  
+        expect(user_administrator).to be_persisted
+        expect(user_administrator.role).to eq('admin')
+      end
+    end
+    context "when the params are invalid" do
+      let(:params) { { email: '', password: '' } }
+      it 'dont create an administrator' do
+        user_administrator = subject.call
+        expect(user_administrator).not_to be_persisted
+      end
     end
   end
 end
