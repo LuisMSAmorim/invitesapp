3702f86c9964e548233b7055e29495604d6cc376
luisMSAmorim <luismiguelsilvaamorim@gmail.com>
2024-02-27 17:44:28 -0300

    feat: invites model validation

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,4 +1,10 @@
 class Invite < ApplicationRecord
   belongs_to :company
   belongs_to :user
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :date, presence: true
+  validates :company, presence: true
+  validates :user, presence: true
 end
