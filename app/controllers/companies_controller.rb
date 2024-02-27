d7c99f3b4ecaa5e8defe9886c924048d3b7572b8
luisMSAmorim <luismiguelsilvaamorim@gmail.com>
2024-02-27 10:30:38 -0300

    feat: companies controller

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,69 +1,53 @@
 class CompaniesController < ApplicationController
-  before_action :set_company, only: %i[ show edit update destroy ]
+  
+  before_action :authenticate_user!
+  before_action :authorize_administrator!
 
-  # GET /companies or /companies.json
   def index
-    @companies = Company.all
+    @companies = Companies::GetAll.new.call
   end
 
-  # GET /companies/1 or /companies/1.json
   def show
+    @company = Companies::GetById.new(params[:id]).call
   end
 
-  # GET /companies/new
+  def edit
+    @company = Companies::GetById.new(params[:id]).call
+  end
+
+  def update
+    @company = Companies::Update.new(params[:id], company_params).call
+
+    if @company.valid?
+      redirect_to companies_path, notice: "Empresa atualizada com sucesso"
+    else
+      flash[:alert] = "Empresa não pode ser atualizada"
+      render :edit
+    end
+  end
+
   def new
     @company = Company.new
   end
 
-  # GET /companies/1/edit
-  def edit
-  end
-
-  # POST /companies or /companies.json
   def create
-    @company = Company.new(company_params)
+    @company = Companies::Create.new(company_params).call
 
-    respond_to do |format|
-      if @company.save
-        format.html { redirect_to company_url(@company), notice: "Company was successfully created." }
-        format.json { render :show, status: :created, location: @company }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @company.errors, status: :unprocessable_entity }
-      end
+    if @company.persisted?
+      redirect_to companies_path, notice: "Empresa criada com sucesso"
+    else
+      flash[:alert] = "Empresa não pode ser criada"
+      render :new
     end
   end
 
-  # PATCH/PUT /companies/1 or /companies/1.json
-  def update
-    respond_to do |format|
-      if @company.update(company_params)
-        format.html { redirect_to company_url(@company), notice: "Company was successfully updated." }
-        format.json { render :show, status: :ok, location: @company }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @company.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /companies/1 or /companies/1.json
   def destroy
-    @company.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to companies_url, notice: "Company was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    Companies::Delete.new(params[:id]).call
+    redirect_to companies_path, notice: "Empresa deletada com sucesso"
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_company
-      @company = Company.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
     def company_params
       params.require(:company).permit(:name, :ceo)
     end
