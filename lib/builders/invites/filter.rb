862147cdaede2c7a9dccc716648b2bbba4e14122
luisMSAmorim <luismiguelsilvaamorim@gmail.com>
2024-02-28 17:53:58 -0300

    feat: invites filter

---
+++ lib/builders/invites/filter.rb
@@ -0,0 +1,34 @@
+module Builders
+  module Invites
+    class Filter
+      def initialize(base_query)
+        @base_query = base_query
+        @filters = []
+      end
+
+      def with_company_id(company_id)
+        @filters.push([:where, company_id: company_id]) if company_id.present?
+        self
+      end
+
+      def with_name(name)
+        @filters.push([:where, 'companies.name ILIKE ?', "%#{name}%"]) if name.present?
+        self
+      end
+
+      def with_inactivation_date(custom_date)
+        return self if custom_date.blank?
+        sanitized_query = ActiveRecord::Base.send(:sanitize_sql_array, ["invites.inactivated_at >= ?", custom_date])
+        @filters.push([:where, sanitized_query])
+        self
+      end
+
+      def call
+        @filters.each do |filter|
+          @base_query = @base_query.send(filter[0], filter[1])
+        end
+        @base_query
+      end
+    end
+  end
+end
