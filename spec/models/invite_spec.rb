6404d082c6a94372448e2e2745c8cff532757d99
luisMSAmorim <luismiguelsilvaamorim@gmail.com>
2024-02-28 21:06:02 -0300

    test: invite model

--- spec/models/invite_spec.rb
+++ spec/models/invite_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Invite, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe 'associations' do
+    it { should belong_to(:company) }
+    it { should belong_to(:user) }
+  end
+  
+  describe 'validations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:date) }
+    it { should validate_presence_of(:company) }
+    it { should validate_presence_of(:user) }
+  end
 end
